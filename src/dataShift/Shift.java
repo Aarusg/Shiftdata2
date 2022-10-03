c4b55a3fbba55fada35fed4262035543e1b5e46e
HP <HP@DESKTOP-NRC>
2022-10-03 12:03:45 +0530

    First data

---
+++ src/dataShift/Shift.java
@@ -0,0 +1,17 @@
+package dataShift;
+
+public class Shift 
+{
+	public static void main(String[] args) 
+	{
+		
+	int	add=m2(20,40);
+		System.out.println(add);
+	
+}
+public static int m2(int a,int b)
+{
+	int c=a+b;
+	return c;
+}
+}
